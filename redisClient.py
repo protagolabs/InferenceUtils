1bebfd94d7fa0833086cef895490541118dc12ae
yuanjie.mao <yuanjie.mao@protagonist-ai.com>
2023-04-10 16:21:12 +0800

    progress debug

--- redisClient.py
+++ redisClient.py
@@ -1,38 +1,57 @@
 import json
 import os
+import time
 
 import redis
+from redis.client import Redis
+from redis.sentinel import Sentinel, MasterNotFoundError, SentinelConnectionPool
 
-host = os.getenv("REDIS_HOST", "localhost")
-password = os.getenv("REDIS_PASSWORD", "")
+password = os.getenv("REDIS_PASSWORD", "8HHKMUuDeYVawngZGMq6")
+sentinel_password = os.getenv("SENTINEL_PASSWORD", "asdfwe90312b123412b12")
 port = os.getenv("REDIS_PORT", 6379)
 stream_name = os.getenv("STREAM_NAME", "DefaultStreamName")
 
-group_name = 'inference_group'
+group_name = "inference_group"
 consumer_name = os.getenv("POD_NAME", "DefaultConsumerName")
 
 
 class RedisClient:
     def __init__(self):
-        pool = redis.ConnectionPool(host=host, port=port, password=password, decode_responses=True)
-        self.client = redis.Redis(connection_pool=pool)
+        sentinel_servers = [("44.215.65.249", 26379),
+                            ("34.230.174.26", 26379),
+                            ("34.230.174.26", 26379)]
+        master_name = "mymaster"
+        sentinel = Sentinel(sentinel_servers, password=password, encoding="utf-8", sentinel_kwargs={"password": sentinel_password})
+        self.client:Redis = sentinel.master_for(master_name, encoding="utf-8",decode_responses=True)
         try:
-            self.client.xgroup_create(stream_name, group_name, id='0', mkstream=True)
+            print(self.client.info())
+        except MasterNotFoundError as e:
+            print("redis sentinel not found , try to connect local redis")
+            self.client = redis.Redis(host="localhost", port=port, password=password)
+        try:
+            self.client.xgroup_create(stream_name, group_name, id="0", mkstream=True)
         except redis.exceptions.ResponseError as e:
             if "Group name already exists" in str(e):
                 print("consumer group already exists")
                 pass
+        print("redis client init finished....")
 
     def get_data_from_stream(self, count=1):
-        message = self.client.xreadgroup(group_name, "consumer_name", {stream_name: '>'}, block=0, count=count)
+        message = self.client.xreadgroup(group_name, "consumer_name", {stream_name: ">"}, block=0, count=count)
         if message:
             return message
         else:
             print("error: get blank message")
 
-    def ack_message(self, message_id_list):
-        for message_id in message_id_list:
-            self.client.xack(stream_name, group_name, message_id)
+    def ack_message(self, message_id):
+        self.client.xack(stream_name, group_name, message_id)
+
+    def set_json_response_to_list(self, list_name, json_data:str):
+        if json_data:
+            self.client.rpush(list_name, json_data)
+            return True
+        else:
+            print("error: json response is blank")
 
     def set_response_to_list(self, list_name, **kwargs):
         if kwargs:
@@ -45,3 +64,8 @@ class RedisClient:
 
     def get_redis_client(self):
         return self.client
+
+    def close(self):
+        self.client.close()
+        print("success to close redis client")
+
--- utils_function.py
+++ utils_function.py
@@ -1,7 +1,12 @@
 import json
 import os
+import shutil
+import signal
+import tarfile
 import tempfile
 import time
+import zipfile
+from pathlib import Path
 from typing import List, Dict
 
 import boto3
@@ -10,7 +15,7 @@ from redisClient import RedisClient
 
 stream_name = os.getenv("STREAM_NAME", "DefaultStreamName")
 
-model_bucket_name = os.getenv("MODEL_BUCKET_NAME", "DefaultModelBucketName")
+model_bucket_name = os.getenv("MODEL_BUCKET_NAME", "netmind-inference-model-bucket")
 
 
 class Utils(object):
@@ -19,31 +24,77 @@ class Utils(object):
         self.redis_client = RedisClient()
         self.s3_client = boto3.Session(region_name="us-west-2").client("s3")
         self.stream_name = stream_name
+        self.should_stop = False
+        signal.signal(signal.SIGTERM, self._sigterm_handler)
 
-    def download_model_and_unzip(self, model_id, use_auth_token=False) -> str:
-        tf = tempfile.NamedTemporaryFile(mode="w+b")
-        self.s3_client.download_fileobj(model_bucket_name, model_id, tf)
-        tf.seek(0)
-        return tf.name
+    def _sigterm_handler(self, signum, frame):
+        print("Received SIGTERM signal, shutting down gracefully...")
+        self.should_stop = True
+
+    def run(self, execute_fuc, interval=0):
+        print("iam ready")
+        while not self.should_stop:
+            execute_fuc()
+            time.sleep(interval)
+        self.redis_client.close()
+        print("worker pod gracefully stopped.")
+
+    def download_and_extract(self, object_key: str) -> str:
+        s3 = boto3.client('s3')
+        tmpdir = tempfile.mkdtemp()
+        file_path = os.path.join(tmpdir, object_key.split('/')[-1])
+        s3.download_file(model_bucket_name, object_key, file_path)
+
+        if file_path.endswith('.zip'):
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                zip_ref.extractall(tmpdir)
+        elif file_path.endswith('.tar.gz') or file_path.endswith('.tar'):
+            with tarfile.open(file_path, 'r:*') as tar_ref:
+                tar_ref.extractall(tmpdir)
+        else:
+            raise ValueError('Unsupported compression format')
+        folder = Path(tmpdir)
+        dir_list = [item for item in folder.iterdir() if item.is_dir() and item.name != "__MACOSX"]
+        if len(dir_list) == 1 and len(list(folder.iterdir())) == 2:
+            print("has sub folder")
+            for sub_item in dir_list[0].iterdir():
+                shutil.move(str(sub_item), str(folder))
+        return tmpdir
 
     def get_input_parameters(self, batch=1) -> List[Dict[str, any]]:
-        messages = self.redis_client.get_data_from_stream(count=batch)
-        input_list = []
-        try:
-            for message in messages[0][1]:
-                input_dict = message[1]
-                input_dict.update({"id": message[0]})
-                if self._is_timeout(input_dict.pop("recordTime")):
-                    input_list.append(input_dict)
-            return input_list
-        except IndexError as e:
-            print("exception happened when get input", e)
-            return []
+        max_retry_times = 10
+        for i in range(max_retry_times):
+            messages = self.redis_client.get_data_from_stream(count=batch)
+            input_list = []
+            try:
+                # messages = [[streamName, [(message_id, {message_body}))}]]]
+                for message in messages[0][1]:
+                    input_dict = message[1]
+                    input_dict.update({"id": message[0]})
+                    if self._is_timeout(input_dict.pop("recordTime")):
+                        input_list.append(input_dict)
+                if not input_list:
+                    continue
+                print("get input", input_list)
+                return input_list
+            except IndexError as e:
+                print("exception happened when get input", e)
+                return []
 
-    def report_response(self, message_id_list: List[str], **kwargs):
-        for message_id in message_id_list:
-            self.redis_client.set_response_to_list(message_id, **kwargs)
-        self.redis_client.ack_message(message_id_list)
+    # def report_response(self, message_id_list: List[str], **kwargs):
+    #     for message_id in message_id_list:
+    #         self.redis_client.set_response_to_list(message_id, **kwargs)
+    #     self.redis_client.ack_message(message_id_list)
+
+    def report_response(self, message_with_id: List[Dict[str, str]]):
+        for message_dict in message_with_id:
+            message_id = next(iter(message_dict))
+            self.redis_client.set_json_response_to_list(message_id, message_dict.get(message_id))
+            self.redis_client.ack_message(message_id)
+
+
+    def inform_ready(self):
+        pass
 
     @staticmethod
     def _is_timeout(start_time, timeout=10):
@@ -52,14 +103,6 @@ class Utils(object):
         else:
             return True
 
-
 if __name__ == '__main__':
-    u = Utils()
-    while True:
-        r = u.get_input_parameters(2)
-        body_list = [json.loads(i.get("body")) for i in r]
-        print(body_list)
-        id_list = [i.get("id") for i in r]
-        time.sleep(2)
-        u.report_response(id_list, rsp="test", rep2="test2")
-
+    utils = Utils()
+    print(utils.get_input_parameters())
